eca14e5955ef8213b79544559f1ef8cdc4adc9cc
mux0x <87925079+mux0x@users.noreply.github.com>
2022-12-27 22:24:48 +0200

    Create crtsh.py

---
+++ crtsh.py
@@ -0,0 +1,36 @@
+import requests
+import argparse
+import sys
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-d", "--domain", help="domain to get it's subs")
+parser.add_argument("-o","--output",help="output file location")
+args = parser.parse_args()
+
+if isinstance(args.domain, str):
+    domain = args.domain
+else:
+    print("you must provide a domain to recon on => python3 crtsh.py -d example.com")
+    sys.exit(0)
+
+if isinstance(args.output, str):
+    outFile = args.output
+else:
+    print("you must provide a file name/path to print the results to => python3 crtsh.py -d example.com -o output.txt")
+
+f = open(outFile, "a")
+try:
+    response = requests.get('https://crt.sh/?q=careem.com&output=json')
+except Exception as e:
+    print("ERROR OCCURED DURING REQUESTING " + e)
+json = response.json()
+subs = []
+for show in json:
+    lol = show['name_value'].split('\n')
+    for x in lol:
+        x = x.replace('*.','')
+        if x not in subs:
+            print("Found: " + x)
+            subs.append(x)
+            f.write(x + '\n')
+f.close()
